68059546333856713b283df8dcb0419f9e96e87e
donggram2 <113027954+donggram2@users.noreply.github.com>
2023-02-01 00:45:09 +0900

    ✨feat: TypeORM moudle 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
 })
 export class AppModule {}
